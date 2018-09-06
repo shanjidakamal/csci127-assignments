b53537632cbf6bf97326c737994c25726c4c7e35
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro.local>
2018-09-06 09:23:58 -0400

    added things

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -2,5 +2,20 @@ def greeter(name):
     return "Hello" +name+" ! "
 print(greeter("Stan"))
 
-def repeat greeter(name):
-    return "Hello" + name+ "!"
+
+def make_abba(abba):
+    a="hello"
+    b="world"
+    result=a+b+b+a
+    return result
+
+print(make_abba("helloworldworldhello"))
+          
+
+def lastFirst(firstName, lastName):
+    separator = ', '
+    result = lastName + separator + firstName
+    return result
+
+print(lastFirst('Shanjida', 'Kamal'))
+print(lastFirst('csci', '127'))
