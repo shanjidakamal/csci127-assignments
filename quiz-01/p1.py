27b1431bcd9e508b3aa98cd24ea50f36ed26111b
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro.local>
2018-09-13 08:42:44 -0400

    function twice

--- quiz-01/p1.py
+++ quiz-01/p1.py
@@ -2,3 +2,8 @@ def didit(name):
     return name + " " + "comepleted the problem"
 
 print (didit("Mike"))
+
+def didit(name):
+    return name + " " + "comepleted the problem"
+
+print (didit("JOHN "))
