7ba583281dca94ba85c756523642e5f82cb4cd2a
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro.local>
2018-09-24 08:55:57 -0400

    FizzBuzz Code

---
+++ lab_02/fizzbuzz.py
@@ -0,0 +1,22 @@
+#Eric Dittus and Shanjida Kamal
+def fizzbuzz(max_value):
+    i = 1
+    count = 0
+    while i <= max_value:
+
+
+        if (i % 5 ==0):
+            if (i % 3 ==0):
+                print ("FizzBuzz")
+                count = count + 1
+            else:
+                print ( "Buzz")
+        elif (i % 3==0):
+            print ("Fizz")
+        
+    
+        else:
+            print (i)
+        i=i+1
+        
+    return count
