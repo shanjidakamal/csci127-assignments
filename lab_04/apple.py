6c38bc966bc5e4b8a25f64efb985f900a782cfa9
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro.local>
2018-11-04 17:38:44 -0500

    apple lab

--- lab_04/apple.py
+++ lab_04/apple.py
@@ -4,14 +4,17 @@ a, b = 5, 15
 apples = [-2, 2, 1]
 oranges = [5, -6]
 
+
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     s_apples = 0
     s_oranges = 0
     for item in apples:
-        if (a + item ) in range(s, t + 1):
+        apple_loc = (a + item)
+        if apple_loc in range(s, t + 1):
             s_apples= s_apples + 1
     for item in oranges:
-        if (b - item) in range(s, t + 1):
+        orange_loc = (b - item)
+        if orange_loc in range(s, t + 1):
             s_oranges= s_oranges + 1           
     return str(s_apples) + "\n" + str(s_oranges)
 
