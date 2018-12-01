6ba10d633be80466ce40c2e5286d982a65880e4a
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro-2.local>
2018-12-01 15:01:50 -0500

    works

---
+++ hw_09/count.cpp
@@ -0,0 +1,18 @@
+//Write a C++ program named count.cpp that uses a while loop to print out 1 through
+//19 on the screen. The output should be the same as or this Python
+//program:
+//   print(i)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 1;
+
+  while (a<20) {
+    cout <<""<<a<<endl;
+    a++;
+  }
+  return 0;
+}
