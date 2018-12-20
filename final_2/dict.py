b6383778bd44b9fbfe34271d4dcbf36cc5c203e3
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro-2.local>
2018-12-20 10:18:23 -0500

    spellcheck func

--- final_2/dict.py
+++ final_2/dict.py
@@ -6,10 +6,26 @@ def addLine(d,line):
         elif item not in d[item[0]]:
             d[item[0]].append(item)
     return d
-d = {"c": ["cat", "cookies"],"m": ["milk"],"s": ["scared"]}
+d = {"c": ["cat", "cookies"],"m": ["milk"],"s": ["scared"], "d":["dog"]} #define the d dictionary so addline function knows how to create the dictionary
 addLine(d, "Santa went down the chimney")
 addLine(d, "He ate some cookies and milk")
 addLine(d, "He also scared the cat and the dog as he put the presents down")
+addLine(d, "As the children came down the stairs, Santa said HOHO and quickly went up the chimney")
 print(d)
 
+print("______________ added more words")#just checking the process 
+addLine(d, "Children were ecstatic when they all opened their presents")
+print(d)
 
+def spellcheck(d,word):
+    word=word.lower()
+    if word[0] in d:
+        if word in d[word[0]]:
+            return True
+    return False
+print("__________") #divide the two parts printing
+print(spellcheck(d, "chimney")) #True
+print(spellcheck(d, "chimnay")) #False
+print(spellcheck(d, "reindeer")) #False
+print(spellcheck(d, "sacred")) #False
+print(spellcheck(d, "cookies")) #True
