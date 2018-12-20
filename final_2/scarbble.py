c946b29b0177a4080e38d6cdf93036442f52f273
Shanjida Kamal <shanjidakamal@Shanjidas-MacBook-Pro-2.local>
2018-12-20 09:47:17 -0500

    added second part

--- final_2/scarbble.py
+++ final_2/scarbble.py
@@ -10,3 +10,16 @@ print(canMakeWord("ladilmy","daily"))
 print(canMakeWord("eerriin","eerie"))
 print(canMakeWord("orrpgma","program"))
 print(canMakeWord("orppgma","program"))
+
+def withWild(letters,word):
+    letters=list(letters)
+    for i in word:
+        if i in  letters:
+            letters.remove(i)
+        elif '?' in letters:
+            letters.remove('?')
+        else:
+            return False
+    return True
+print(withWild("oo?m","mood"))
+print(withWild("cava?ion","vacation"))
